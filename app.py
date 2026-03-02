2dd7196a8ce6e93a7f6a452076edbe04165f97bc
Tiago Ribeiro <tiagoribeirodev@gmail.com>
2026-03-02 08:17:17 -0300

    teste id.

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
 st.set_page_config(
-    page_title="Dashboard de Campanhas - SICOOB COCRED - Id", 
+    page_title="Dashboard de Campanhas - SICOOB COCRED - Id.", 
     layout="wide",
     page_icon="📊",
     initial_sidebar_state="expanded"
